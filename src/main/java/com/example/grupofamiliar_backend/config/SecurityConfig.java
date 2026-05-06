43b9ccd1e030ef5f31fdd2fdc88688f9f1145afc
pablojc3005 <pablojc3005@gmail.com>
2026-05-06 16:46:03 -0500

    cambio en securityconfig

--- src/main/java/com/example/grupofamiliar_backend/config/SecurityConfig.java
+++ src/main/java/com/example/grupofamiliar_backend/config/SecurityConfig.java
@@ -114,7 +114,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("http://grupofamiliarbackend-production.up.railway.app:8080", "http://localhost:4200",
+                Arrays.asList("http://grupofamiliarbackend-production.up.railway.app", "http://localhost:4200",
                         "http://localhost:5173",
                         "http://localhost:5174"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
